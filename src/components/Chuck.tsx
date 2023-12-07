d688b4ce23c962d0c2fc32a4750b2893b7b4552f
Fran-Tera <fran.ribeiro@teratelemedicina.com.br>
2023-12-07 15:22:10 -0300

    :construction: add no cors

--- src/components/Chuck.tsx
+++ src/components/Chuck.tsx
@@ -15,7 +15,7 @@ export default async function Chuck() {
   // try {
   const response = await fetch(`https://api.chucknorris.io/jokes/random`, {
     method: "GET",
-    next: { revalidate: 30 },
+    // next: { revalidate: 30 },
   });
 
   if (!response.ok) {
