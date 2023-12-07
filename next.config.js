07508c39ade0f0267e4a0c0e16388e8613f9a73c
Fran Ribeiro <ribeirofrv@gmail.com>
2023-12-07 10:42:58 -0300

    :construction: test pr (#1)
    :construction: test
    
    Co-authored-by: Fran-Tera <fran.ribeiro@teratelemedicina.com.br>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['assets.chucknorris.host'],
+      },
+}
 
 module.exports = nextConfig
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Chuck from "../components/Chuck";
+import Form from "../components/Form";
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      {/* <Chuck /> */}
+      <Form />
+    </main>
+  );
+}
